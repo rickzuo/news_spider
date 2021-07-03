8a1dee0a12d49ca5a0e62c88560fd66181de5559
zuoak <zuoak@jiguang.cn>
2021-07-03 09:20:00 +0800

    fix 36kr rank

--- main.py
+++ main.py
@@ -5,5 +5,5 @@
 # @Software: PyCharm
 from scrapy.cmdline import execute
 
-execute(['scrapy', 'crawl', 'juejin'])
+execute(['scrapy', 'crawl', '36kr'])
 # execute(['scrapy', 'crawl_all'])
--- news_spider/spiders/kr36.py
+++ news_spider/spiders/kr36.py
@@ -1,3 +1,6 @@
+import json
+import logging
+
 import scrapy
 
 from news_spider.items import Kr36Item
@@ -6,13 +9,42 @@ from news_spider.utils.common import get_category_by_name, parse_list_item
 
 class A36krSpider(scrapy.Spider):
     name = '36kr'
-    allowed_domains = ['www.36kr.com']
+    allowed_domains = ['www.36kr.com/newsflashes']
     start_urls = ['https://36kr.com/newsflashes']
     category_id = get_category_by_name(name)
 
     def parse(self, response):
+        script_list = response.xpath("//script[contains(text(),'window.initialState=')]")
+        if not script_list:
+            logging.error("crawl 36kr fail:", script_list)
+            return
+
+        script_data = script_list[0].extract()
+        import re
+        re_data = re.findall("initialState=(.*?)</script>",script_data,re.S)
+        script_data = re_data[0]
+        script_json = json.loads(script_data)
+        item_list = script_json["newsflashCatalogData"]["data"]["newsflashList"]["data"]["itemList"]
+        for data in item_list:
+            item = Kr36Item()
+            template_material = data["templateMaterial"]
+            title = template_material["widgetTitle"]
+            ptime = template_material["publishTime"]
+            item_id = template_material["itemId"]
+            widget_content = template_material["widgetContent"]
+
+            item["title"] = title
+            url = f"{self.allowed_domains[0]}/{item_id}"
+            item["url"] = url
+            item["rank"] = ptime
+            item["category_id"] = self.category_id
+            yield item
+
+
+
+    def parse2(self, response):
         news_list = response.xpath("//div[@class='newsflash-item']")
-        for index, new in enumerate(news_list):
+        for new in news_list:
             item = Kr36Item()
             href = new.xpath("./a[@class='item-title']/@href").extract()
             title = new.xpath("./a[@class='item-title']/text()").extract()
@@ -21,7 +53,7 @@ class A36krSpider(scrapy.Spider):
             item["title"] = title
             url = f"{self.allowed_domains[0]}{url_path}"
             item["url"] = url
-            item["rank"] = index + 1
+            item["rank"] = 0
             item["category_id"] = self.category_id
             # href = new.xpath("./div[@class='item-desc']/a[@class='link']/@href").extract()
             print(url, title)
